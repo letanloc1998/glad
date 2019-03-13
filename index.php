3376cfb8d7c16d90cf022db1a7ce40cea0675e8a
letanloc1998 <letanloc1998@gmail.com>
2019-03-14 03:26:38 +0700

    Gtmetrix

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
         <title>Trang chủ</title>
         <link rel="shortcut icon" href="img/logo.png">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--
         <link rel="stylesheet" type="text/css" media="screen" href="index.css">
         <script src="index.js"></script>
+        -->
+
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({
@@ -15,6 +19,19 @@
                 enable_page_level_ads: true
             });
         </script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134530849-4"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-134530849-4');
+        </script>
+
     </head>
 
     <body>
