6727de2d5673535f7e5215c55fbdf4dce6f14711
letanloc1998 <letanloc1998@gmail.com>
2019-03-14 03:05:23 +0700

    Update index.php
    Update google ads

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Trang chủ</title>
-    <link rel="shortcut icon" href="img/logo.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="index.css">
-    <script src="index.js"></script>
-</head>
-<body>
-    
-    <h1>Trang chủ</h1>
 
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Trang chủ</title>
+        <link rel="shortcut icon" href="img/logo.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="index.css">
+        <script src="index.js"></script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-2817105554366366",
+                enable_page_level_ads: true
+            });
+        </script>
+    </head>
+
+    <body>
+
+        <h1>Trang chủ</h1>
+
+    </body>
+
 </html>
