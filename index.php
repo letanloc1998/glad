d59e66d1c44294919b0b6037f79980fe6707c96f
letanloc1998 <letanloc1998@gmail.com>
2019-03-16 20:22:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
     header("Pragma: no-cache"); //HTTP 1.0
     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60)));
 
     //or, if you DO want a file to cache, use:
     //header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
